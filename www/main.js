8dc41be5b3ca1b6c817302c5a5158037ef7c3b53
Denys Myronov <mironov.link@gmail.com>
2020-04-26 20:21:36 +0300

    Added method to design

--- www/main.js
+++ www/main.js
@@ -1,1 +1,36 @@
-main.js
+$(document).ready(main);
+
+function initOptions() {
+
+}
+
+function main() {
+
+}
+
+function parseParameters() {
+	return {
+		url: "",
+		method: "",
+		headers: {},
+		body: ""
+	};
+}
+
+function sendClick() {
+
+}
+
+function getOptionValue(code) {
+
+}
+
+function createOption(code) {
+
+/*
+	<div class="option-row" data-code="somecode">
+		<div class="option-name">Enable base64</div>
+		<input type="checkbox" class="option-value">
+	</div>
+*/
+}
